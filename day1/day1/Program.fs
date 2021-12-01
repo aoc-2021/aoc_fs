97b5878bd79515d4272e647704143ffa798eb80c
xeno <xeno@lv427.space>
2021-12-01 08:00:02 +0100

    some refactoring

--- day1/day1/Program.fs
+++ day1/day1/Program.fs
@@ -14,17 +14,16 @@ let slides =
    let slideSum n = depths.[n] + depths.[n-1] + depths.[n-2]
    indexes |> Seq.map slideSum |> Seq.toArray 
    
-let deltas = seq { 1 .. (depths.Length-1) }
 
-let slideDeltas = seq { 1 .. (slides.Length-1) }
-
-let delta  (depths:array<int>) (i:int): int = depths.[i] - depths.[i-1]
-let diffs = deltas |> Seq.map (delta depths) |> Seq.toArray
-let slideDiffs = slideDeltas |> Seq.map (delta slides) |> Seq.toArray 
+let delta (depths:array<int>) (i:int): int = depths.[i] - depths.[i-1]
+let diffs (depths:array<int>) =
+    let deltas = seq { 1 .. (depths.Length-1) }
+    let delta (i:int): int = depths.[i] - depths.[i-1]
+    deltas |> Seq.map delta |> Seq.toArray
 
 let pos x = x > 0
-let incs = diffs |> Seq.filter pos |> Seq.toArray |> Array.length
-let slideIncs = slideDiffs |> Seq.filter pos |> Seq.toArray |> Array.length 
+let incs = diffs depths |> Seq.filter pos |> Seq.toArray |> Array.length
+let slideIncs = diffs slides |> Seq.filter pos |> Seq.toArray |> Array.length 
 
 printfn $"Solution 1: %A{incs}"
 printfn $"Solution 2: %A{slideIncs}"
