ea650c3a7bddf4bf1e7769bff3cda3fa0a009e63
xeno <xeno@lv427.space>
2021-12-06 18:32:51 +0100

    various cleanups

--- day6/Program.fs
+++ day6/Program.fs
@@ -4,52 +4,38 @@ let file = File.ReadAllLines "input.txt"
 
 let fish = file.[0].Split ',' |> Array.map int
 
-printfn $"fish {fish}"
-
-let toCount (x:(int*int[])) =
-   let (fish,fishes) = x
-   (fish,fishes.Length |> int64)
+let toCount ((fish,fishes):(int*int[])) = (fish,fishes.Length |> int64)
 let fishCount:(int*int64)[] = fish |> Array.groupBy id  |> Array.map toCount
 
-printfn $"{fishCount |> Array.toList}"
-
 let getCountForFish (n:int) (fish:Map<int,int64>) =
-   match fish.TryFind n with
-      | None -> 0L
-      | Some(fishCount) -> fishCount 
-
-let removeFish (fishNo:int) (fish:Map<int,int64>) =
-   fish.Remove fishNo
-
-let addNewFish (fishNo:int) (n:int64) (fish:Map<int,int64>) =
-   if getCountForFish fishNo fish > 0L then failwith $"Fish already exists: {fishNo}"
-   fish.Add (fishNo,n) 
+   fish.TryFind n |> Option.defaultValue 0L
 
 let addToFish (fishNo:int) (n:int64) (fish:Map<int,int64>) =
    let current = getCountForFish fishNo fish
    let newCount = current + n
-   let withoutFish = removeFish fishNo fish
-   addNewFish fishNo newCount withoutFish
+   let withoutFish = fish.Remove fishNo 
+   withoutFish.Add (fishNo,newCount)
 
 let countDownAll (fish:Map<int,int64>) : Map<int,int64> =
-   fish |> Map.toSeq |> Seq.map (fun f -> ((fst f)-1),(snd f)) |> Map 
+   let decKey (fish,count) = (fish-1,count)
+   fish |> Map.toSeq |> Seq.map decKey |> Map
 
-let fIter (fish:(int*int64)[]) =
-   let zeroes = getCountForFish 0 (Map fish)
-   let fish = removeFish 0 (Map fish)
+let fIter (fish:Map<int,int64>) =
+   let zeroes = getCountForFish 0 fish
+   let fish = fish.Remove 0
    let fish = addToFish 7 zeroes fish
    let fish = countDownAll fish
-   let fish = addNewFish 8 zeroes fish 
-   fish |> Map.toArray 
+   let fish = fish.Add (8,zeroes)
+   fish 
    
-let rec fIterN (n:int) (fish:(int*int64)[]) =
+let rec fIterN (n:int) (fish:Map<int,int64>) =
    if n = 0 then fish
    else fIterN (n-1) (fIter fish)
 
-let cFish80 = fIterN 80 fishCount 
-let cFish256 = fIterN 256 fishCount 
+let cFish80 = fIterN 80 (Map fishCount) 
+let cFish256 = fIterN 256 (Map fishCount)
 
-let count fish = fish |> Array.map snd |> Array.sum
+let count fish = fish |> Map.toSeq |> Seq.map snd |> Seq.sum
 
 printfn $"Part 1 (80 iterations): {count cFish80}"
 printfn $"Part 2 (256 iterations): {count cFish256}"
