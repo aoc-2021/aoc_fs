789e749ce445045561b56799d1911dacef91e1dc
xeno <xeno@lv427.space>
2021-12-11 07:50:25 +0100

    minor cleanup

--- day11/Program.fs
+++ day11/Program.fs
@@ -1,26 +1,10 @@
-open System.Diagnostics.CodeAnalysis
 open System.IO
-open System 
 
 let input = File.ReadAllLines "input.txt"
 
-let input2 = [|
-"5483143223";
-"2745854711";
-"5264556173";
-"6141336146";
-"6357385478";
-"4167524645";
-"2176841721";
-"6882881134";
-"4846848554";
-"5283751526"|]
-
-let lines = input
-
 let octopuses =
     let toInt c = (int c) - (int '0')
-    lines |> Array.map (fun s -> s.ToCharArray () |> Array.map toInt)
+    input |> Array.map (fun s -> s.ToCharArray () |> Array.map toInt)
 
 type Point = int*int
 
@@ -45,15 +29,11 @@ let allPoints : list<Point> =
     let ys = {0..9}
     xs |> Seq.map (fun x -> ys |> Seq.map (fun y -> (x,y)) |> Seq.toList) |> Seq.toList |> List.concat
     
-printfn $"all points: {allPoints}"
-
 let buildMap (octopuses:int[][]) : Map<Point,int> =
     let toRef ((x,y):Point) = (x,y),octopuses.[y].[x]
     allPoints |> List.map toRef |> Map
     
-let initMap = buildMap octopuses |> Octomap 
-
-printfn $"initMap={initMap}"
+let initOctos = buildMap octopuses |> Octomap 
 
 let neighbors ((x,y):Point) : List<Point> =
     [(x-1,y-1);(x,y-1);(x+1,y-1);
@@ -62,7 +42,6 @@ let neighbors ((x,y):Point) : List<Point> =
 
 let step (octos:Octomap) : Octomap*int =
     let octos = octos.IncByOne()
-    printfn $"inced {octos}"
     let rec flashOff (flashed:Set<Point>) (octos:Octomap) : Octomap*int =
         let flashers = octos.Charged () |> Set.filter (fun p -> flashed.Contains p |> not)
         if flashers.IsEmpty then octos,flashed.Count
@@ -75,27 +54,27 @@ let step (octos:Octomap) : Octomap*int =
     let octos = octos.Defuse ()
     octos,flashCount 
     
-let octos1,flashCount = step initMap 
+let octos1,flashCount = step initOctos 
 
 let octos99 = {1..99} |> Seq.fold (fun (octos,c) n ->
     let (nextOctos,newFlashes) = step octos
-    (nextOctos,newFlashes+c)) (initMap,0)
+    (nextOctos,newFlashes+c)) (initOctos,0)
 
 let stepN (n:int) (octos:Octomap) =
    {1..n} |> Seq.fold (fun (octos,c) n ->
        let (octos,newFlashes) = step octos
        octos,newFlashes+c) (octos,0)
    
-printfn $"99: {stepN 100 initMap}"
+printfn $"Task 1 {stepN 100 initOctos |> snd}"
 
 // part 2
 let rec findFullFlash (octos:Octomap) (stepcount:int) : int =
+    let stepcount = stepcount + 1
     let octos,flashes = step octos
-    printfn $"{stepcount}:{flashes}"
     if flashes < 100 then
-        findFullFlash octos (stepcount+1)
+        findFullFlash octos stepcount
     else 
-        stepcount+1 
+        stepcount
 
-let n = findFullFlash initMap 0
-printfn $"solution: {n}"
+let brightStep = findFullFlash initOctos 0
+printfn $"Task 2: {brightStep}"
