2ecf29a38f0cb03a8806be509243889cf0201085
xeno <xeno@lv427.space>
2021-12-12 11:52:44 +0100

    optimization, using ints instead of cave names

--- day12/Program.fs
+++ day12/Program.fs
@@ -1,19 +1,35 @@
 open System
 open System.IO
 
-let input = File.ReadAllLines "input.txt" |> Array.toList 
+let input = File.ReadAllLines "input.txt" |> Array.toList
 
-type Cave = string
+type Cave = int
 type Tube = Cave*Cave
 
-let allTubesForward: Set<Tube> = input |> List.map (fun s -> s.Split '-' |> fun s -> (s.[0],s[1])) |> Set
+let stringTubes = input |> List.map (fun s -> s.Split '-')
+
+let caveValueMap =
+    let names = stringTubes |> List.map Array.toList |> List.concat |> Set |> Set.toArray
+    let isUpperCase (s:string) = s.ToCharArray () |> Array.exists Char.IsUpper 
+    names |> Array.mapi (fun i v -> (v,i* if isUpperCase v then -1 else 1))
+    |> Map 
+
+printfn $"caveValueMap {caveValueMap}"    
+
+let allTubesForward: Set<Tube> =
+    let toCave (s:string) = caveValueMap.TryFind s |> Option.get
+    stringTubes |> List.map (fun s -> (s.[0] |> toCave,s.[1] |> toCave)) |> Set
+    
+let START = caveValueMap.TryFind "start" |> Option.get 
+let END = caveValueMap.TryFind "end" |> Option.get 
+    
 let allTubesBackward: Set<Tube> = allTubesForward |> Set.map (fun (a,b) -> (b,a))
 
 let allTubes = Set.union allTubesForward allTubesBackward
 
-let starts = allTubes |> Set.filter (fun (b,_) -> b = "start")
+let starts = allTubes |> Set.filter (fun (b,_) -> b = START)
 
-let tubes = allTubes |> Set.filter (fun (a,b) -> a = "start" || b = "start" |> not)
+let tubes = allTubes |> Set.filter (fun (a,b) -> a = START || b = START |> not)
 
 type CaveMap = Map<Cave,list<Cave>>
 let caveMap : CaveMap =
@@ -21,13 +37,13 @@ let caveMap : CaveMap =
               |> List.map (fun (cave,tubes) -> (cave,(tubes |> List.map snd)))
               |> Map 
 
-let isSmall (cave:Cave) = cave.ToCharArray () |> Array.exists Char.IsUpper |> not 
+let isSmall (cave:Cave) = cave > -1 
 
-let rec expand (denySecondVisits:bool) (visitedSmall:Set<string>) (caveMap:CaveMap) (e:Cave) =
+let rec expand (denySecondVisits:bool) (visitedSmall:Set<int>) (caveMap:CaveMap) (e:Cave) =
     let secondVisit = isSmall e && visitedSmall.Contains e
     let visitedSmall = if isSmall e then visitedSmall.Add e else visitedSmall 
     if secondVisit && denySecondVisits then []
-    else if e = "end" then [[e]]
+    else if e = END then [[e]]
     else
         let denySecondVisits = denySecondVisits || secondVisit
         let nexts = caveMap.TryFind e |> Option.defaultValue []
