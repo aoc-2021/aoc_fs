0811adc7524f9d3934893e8d8c343282c320de81
xeno <xeno@lv427.space>
2021-12-13 07:01:39 +0100

    day 13

--- day13/Program.fs
+++ day13/Program.fs
@@ -25,7 +25,7 @@ let input2 = ["6,10";
 "fold along y=7";
 "fold along x=5"]
 
-let input = input2
+let input = input1
 
 let rec getPoints(input:list<string>) =
     match input with
@@ -39,9 +39,56 @@ let rec getFolds (input:list<string>) =
         let fold = (s.Split ' ').[2].Split '='
         ((fold.[0].ToCharArray ()).[0],fold.[1] |> int)
     input |> List.filter (fun s -> s.StartsWith "fold") |> List.map getFold 
-        
+
+type Point = int*int
+type Paper = List<Point> 
 let startPaper = getPoints input
 let folds = getFolds input
 
 printfn $"startPaper {startPaper}"
 printfn $"folds {folds}"
+
+let foldY (paper:Paper) (line:int) : Paper =
+    let foldP ((x,y):Point) =
+        if y < line then (x,y)
+        else  (x,line - (y - line))
+    paper |> List.map foldP
+
+let foldX (paper:Paper) (line:int) : Paper =
+    let foldP ((x,y):Point) =
+        if x < line then (x,y)
+        else  (line - (x - line),y)
+    paper |> List.map foldP
+    
+let rmDupl (list:List<Point>) = list |> Set |> Set.toList
+let fold (paper:Paper) ((axis,line):char*int) : Paper =
+    let points = match axis with
+                 | 'x' -> foldX paper line 
+                 | 'y' -> foldY paper line
+    rmDupl points 
+    
+
+let paper1 = fold startPaper (folds.Head) 
+
+printfn $"{paper1.Length}"
+
+let folded = folds |> List.fold (fun (paper:Paper) (f:char*int) -> fold paper f) startPaper
+
+let paperPts = folded |> Set 
+let ys = {1..10}
+let xs = {1..80}
+
+let coords : List<int*int> = {0..10} |> Seq.map (fun y -> {0..60} |> Seq.map (fun x -> (x,y)))
+                             |> Seq.concat 
+                             |> Seq.toList
+
+coords |> List.map
+              (fun point -> 
+    match point with
+    | (60,_) -> printfn ""
+    | p when paperPts.Contains p -> printf "█"
+    | p -> printf " ")
+    
+
+
+
