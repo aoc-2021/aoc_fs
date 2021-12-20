bb123e6490eb80fc1409f54626950b1b385e7067
xeno <xeno@lv427.space>
2021-12-20 07:59:02 +0100

    solved day 20

--- day20/Program.fs
+++ day20/Program.fs
@@ -19,7 +19,7 @@ type Frame(x1: int64, y1: int64, x2: int64, y2: int64) =
         Seq.allPairs { x1 .. x2 } { y1 .. y2 }
         |> Seq.toList
     member this.Expand () : Frame =
-        Frame(x1-1L,y1-1L,x2+1L,y2+1L)
+        Frame(x1-2L,y1-2L,x2+2L,y2+2L)
     
     override this.ToString () =
         $"Frame{(x1,y1,x2,y2)}"
@@ -28,31 +28,32 @@ type Enhancer(digits: Digit []) =
     member this.Digits = digits
     member this.GetPixel(index: int64) = digits.[index |> int]
 
-type Image(image: Map<Pos, int64>, frame: Frame) =
+type Image(image: Map<Pos, int64>, frame: Frame, def:Digit) =
     member this.Image = image
     member this.Frame = frame
 
     member this.getPixel(pos: Pos) : Digit =
-        image.TryFind pos |> Option.defaultValue 0L
+        image.TryFind pos |> Option.defaultValue def 
 
     member this.getAreaPixels((x, y): Pos) : list<Digit> =
         let pixels =
             [ (x - 1L, y - 1L)
-              (x, y - 1L)
-              (x, y + 1L)
+              (x     , y - 1L)
+              (x + 1L, y - 1L)
               (x - 1L, y)
-              (x, y)
+              (x     , y)
               (x + 1L, y)
               (x - 1L, y + 1L)
-              (x, y + 1L)
+              (x     , y + 1L)
               (x + 1L, y + 1L) ]
 
         pixels |> List.map this.getPixel
+    member this.Def = def 
         
     override This.ToString () =
         $"Image({image} {frame})"
         
-let blankImage = Image(Map.empty, Frame(0L, 0L, 0L, 0L))
+// let blankImage = Image(Map.empty, Frame(0L, 0L, 0L, 0L))
 
 let binaryToInt (bin: list<Digit>) : int64 =
     let rec addBinaryDigit (acc: int64) (bin: list<Digit>) =
@@ -90,11 +91,11 @@ let readInput (input: list<string>) =
         |> List.map (fun (x, y) -> ((x, y), imageArray.[y |> int].[x |> int]))
         |> Map
 
-    let frame = Frame(-1L, -1L, maxX + 1L, maxY + 1L)
-    let image = Image(allPixels, frame)
+    let frame = Frame(-2L, -2L, maxX + 2L, maxY + 2L)
+    let image = Image(allPixels, frame,0L)
     enhancer, image
 
-let input = testFile
+let input = file
 
 let enhancer, image = readInput input
 
@@ -109,12 +110,37 @@ let enhance (enhancer: Enhancer) (image: Image) : Image =
                   |> List.map (fun (pos,index) -> (pos,enhancer.GetPixel index))
                   |> Map
     let frame = image.Frame.Expand ()
-    Image(nextMap,frame)
+    let def = image.getAreaPixels (10000000000L,100000000L) |> binaryToInt |> int64 |> enhancer.GetPixel
+    // printfn $"Default: {def}"
+    Image(nextMap,frame,def)
     
 let image1 = enhance enhancer image
 let image2 = enhance enhancer image1
 
+let printImage (image:Image) =
+    let toPrintable (pixel:Digit) = if pixel = 1L then '#' else '.'
+    printfn $"Printing image {image}"
+    let ys = {image.Frame.Y1..image.Frame.Y2} |> Seq.toList 
+    let xs = {image.Frame.X1..image.Frame.X2} |> Seq.toList 
+    ys |> List.map (fun y ->
+            xs |> List.map (fun x -> printf $"{image.getPixel(x,y) |> toPrintable}")
+            printfn ""                
+        )
+    printfn ""  
+
 let countLit (image:Image) : int =
     image.Image.Values |> Seq.filter ((=) 1L) |> Seq.length
 
-printfn $"image@1: {image2} : lit = {countLit image2}"
+printImage image 
+printImage image1
+printImage image2
+
+printfn $"image@2: {image2} : lit = {countLit image2}"
+
+printfn $"image:(1,2) = {image.getPixel (1L,2L)}"
+
+printfn $"{image.getAreaPixels (2L,2L) |> binaryToInt}"
+
+let image50 = {0..49} |> Seq.toList |> List.fold (fun image _ -> enhance enhancer image) image 
+
+printfn $"image50 {countLit image50}"
