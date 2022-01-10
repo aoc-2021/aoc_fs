33add52d3da8ce9066b0961369fde79ddef99d98
xeno <xeno@lv427.space>
2022-01-10 11:35:04 +0100

    ...

--- day24_try10/Program.fs
+++ day24_try10/Program.fs
@@ -37,7 +37,8 @@ let rec valueToString (value: Value) =
 let toSequence (value:Value) =
     match value with
     | CONST c -> SEQUENCE [CONST c]
-    | RANGE(a,b) -> [a..b] |> List.map CONST |> SEQUENCE 
+    | RANGE(a,b) -> [a..b] |> List.map CONST |> SEQUENCE
+    | _ -> failwith $"Not implemented: toSequence {value}"
 
 let expand (value:Value) =
     match value with
@@ -84,6 +85,7 @@ let toBoolean (value:Value) : Value =
     | false,false -> EMPTY 
 
 let rec intersection (value1: Value) (value2: Value) : Value =
+    // printfn $"intersection {value1} {value2}"
     match value1, value2 with
     | UNKNOWN, _ -> value2
     | _, UNKNOWN -> value1
@@ -120,7 +122,7 @@ let rec intersection (value1: Value) (value2: Value) : Value =
             | _,[] -> []
             | RANGE(a,b)::rest1,_ when a = b -> interSeq ((CONST a)::rest1) seq2
             | _,RANGE(a,b)::rest2 when a = b -> interSeq seq1 ((CONST a)::rest2)
-            | CONST a::rest1,CONST b::_ when a < b -> interSeq rest1 (seq2)
+            | CONST a::rest1,CONST b::_ when a < b -> interSeq rest1 seq2
             | CONST a::_,CONST b::rest2 when a > b -> interSeq seq1 rest2
             | CONST a::rest1,CONST b::rest2 when a = b -> CONST a :: (interSeq rest1 rest2)
             | CONST c::rest1,RANGE(a,_)::_ when c < a -> interSeq rest1 seq2
@@ -140,6 +142,7 @@ let rec intersection (value1: Value) (value2: Value) : Value =
         list |> List.map (fun e -> intersection e (FROM c)) |> List.filter ((<>) EMPTY) |> SEQUENCE
     | RANGE _, SEQUENCE _ -> intersection (SEQUENCE [value1]) value2 
     | FROM a, FROM b -> FROM (max a b)
+    | FROM a, CONST c -> if a <= c then CONST c else EMPTY 
     | _ -> failwith $"intersection: Not implemented: {value1} {value2}"
 
 
@@ -210,7 +213,8 @@ let rec addValue (value: Value) (i: int64) =
     match value with
     | UNKNOWN -> UNKNOWN
     | CONST c -> CONST (c + i) 
-    | SEQUENCE seq -> seq |> List.map (fun e -> addValue e i) |> SEQUENCE 
+    | SEQUENCE seq -> seq |> List.map (fun e -> addValue e i) |> SEQUENCE
+    | RANGE (a,b) -> RANGE (a+i,b+i) 
     | _ -> failwith $"Not implemented: + {value} {i}"
 
 let mulValue (value: Value) (i: int64) : Value =
@@ -230,7 +234,28 @@ let rec eqValue (value1: Value) (value2: Value) : Value =
               | _,UNKNOWN -> ONE_AND_ZERO
               | _,_ -> failwith $"Not implemented EQ {value1} {value2}"
     // printfn $"eqValue {value1} {value2} -> {res}"
-    res 
+    res
+let expandSequence (value:Value) =
+    let valueToSet (value:Value) =
+        match value with
+        | CONST c -> Set.singleton c
+        | RANGE (a,b) -> [a..b] |> Set 
+    match value with
+    | SEQUENCE list ->
+        list |> List.map valueToSet |> Set.unionMany |> Set.toList |> List.sort |> List.map CONST |> SEQUENCE 
+    | _ -> failwith $"expandSequence {value}"
+
+let rec modValue (value:Value) (i:int64): Value =
+    match value with
+    | CONST c -> CONST (c % i)
+    | RANGE (a,b) when a >= 0L && b-a > i -> RANGE(0,i-1L)
+    | RANGE (a,b) when a >= 0L && b < i -> value 
+    | SEQUENCE l ->
+        l |> List.map (fun e -> modValue e i)
+          |> SEQUENCE |> expandSequence
+    | FROM _ -> RANGE (0,i-1L)
+    | TO _ -> RANGE (0,i-1L)
+    | _ -> failwith $"Not implemented: modValue {value} {i}"
 
 let rec minValue (value:Value) : int64 =
     match value with
@@ -240,9 +265,23 @@ let rec minValue (value:Value) : int64 =
     | FROM a -> a
     | _ -> failwith $"minValue: unsupported: {value}"
 
+let rec maxValue (value:Value) : int64 =
+    match value with
+    | CONST c -> c
+    | RANGE (_,b) -> b
+    | SEQUENCE list ->
+        let rec last (list:list<Value>) =
+            match list with
+            | [] -> failwith $"maxValue: failed to get max from {value}"
+            | [e] -> maxValue e
+            | _::tail -> last tail
+        last list 
+
+if maxValue (SEQUENCE [RANGE (1L,3L);RANGE(5L,9L)]) <> 9L then
+    failwith $"maxValue doesn't work"
+
 let rec narrowValues (op: Op) (param1: Value) (param2: Value) (result: Value) : Op * Value * Value * Value =
     // printfn $"narrow {op} {param1 |> valueToString} {param2 |> valueToString} {result |> valueToString}"
-    printfn 
     let skipSilent() =
         op,param1,param2,result
     let param1 = consolidate param1
@@ -265,6 +304,9 @@ let rec narrowValues (op: Op) (param1: Value) (param2: Value) (result: Value) : 
         // TODO: narrow param1
         ADD,param1,param2,result
     | ADD,CONST a,CONST b,_ -> ADD,param1,param2,intersection (CONST (a+b)) result
+    | ADD,_,CONST c,CONST r ->
+        let param1 = intersection (CONST (r-c)) param1
+        ADD,param1,param2,result
     | ADD,SEQUENCE _,CONST i,_ ->
         let result = intersection (addValue param1 i) result
         let param1 = addValue result (-i)
@@ -272,6 +314,10 @@ let rec narrowValues (op: Op) (param1: Value) (param2: Value) (result: Value) : 
     | ADD,FROM _,UNKNOWN,FROM _ -> skipSilent ()
     | ADD,FROM a,UNKNOWN,CONST c ->
         let param2 = TO (c-a)
+        ADD,param1,param2,result
+    | ADD,FROM a,SEQUENCE _,FROM b ->
+        let param1 = intersection (FROM (b-(maxValue param2))) param1
+        let result = intersection (FROM (a+(minValue param2))) result
         ADD,param1,param2,result
     | DIV,UNKNOWN,_,UNKNOWN -> skipSilent () 
     | DIV,_,CONST 1L,_ ->
@@ -304,10 +350,14 @@ let rec narrowValues (op: Op) (param1: Value) (param2: Value) (result: Value) : 
         let param2 = pairs |> List.map snd |> Set |> Set.toList |> List.sort |> List.map CONST |> SEQUENCE |> consolidate
         let result = pairs |> List.map (fun (a,b) -> a*b) |> Set |> Set.toList |> List.sort |> List.map CONST |> SEQUENCE |> consolidate
         MUL,param1,param2,result  // no need to intersect with result, already checked 
-    | MOD,CONST 0L,CONST c,_ -> MOD,param1,param2,intersection result (CONST 0L)
+    | MOD,CONST 0L,CONST _,_ -> MOD,param1,param2,intersection result (CONST 0L)
     | MOD,UNKNOWN,_,_ -> MOD,FROM 0L,param2,result 
     | MOD,UNKNOWN,CONST c,_ -> MOD,param1,param2,intersection (RANGE (0L,c-1L)) result
-    | MOD,FROM _,_,_ -> MOD,param1,param2,result 
+    | MOD,FROM _,_,_ -> MOD,param1,param2,result
+    | MOD,SEQUENCE _, CONST c,_ ->
+        let result = intersection (modValue param1 c) result
+        printfn $"TODO: narrow {param1} <- {result}"
+        MOD,param1,param2,result
     | EQL,UNKNOWN,UNKNOWN,_ -> EQL,UNKNOWN,UNKNOWN,toBoolean result 
     | EQL,UNKNOWN,_,UNKNOWN -> EQL,param1,param2,ONE_AND_ZERO
     | EQL,_,_,_ when isOneAndZero result -> EQL,param1,param2,toBoolean result 
@@ -435,7 +485,7 @@ let task1iter (program: Program) =
     program
 
 let task1 (program: Program) =
-    { 1 .. 18 }
+    { 1 .. 50 }
     |> Seq.fold (fun program i ->
                                  printfn $"### ITER {i} ###"
                                  task1iter program) program
