1ce7aaa63d44f19395e221908e9e1ec1db4f0412
xeno <xeno@lv427.space>
2022-01-13 09:39:19 +0100

    ...

--- day24_try13_1/Program.fs
+++ day24_try13_1/Program.fs
@@ -8,18 +8,59 @@ type Reg =
     | Y
     | Z
 
+
+
+type Sources(inputs:Map<int,Set<int64>>) =
+    member this.Inputs = inputs
+    override this.ToString() =
+        let inputsToString (is:Set<int64>) = is |> Set.toList |> List.map string |> String.concat ""
+        let pairToString (k,vs) = $"{k}:{vs|>inputsToString}"
+        inputs |> Map.toList |> List.map pairToString |> String.concat ","
+
+type SourcedNumber (value:int64,sources:Sources)  =
+    interface System.IComparable with
+        override this.CompareTo(other) =
+            match other with 
+            | :? SourcedNumber as other ->
+                if value > other.Value then 1
+                else if value < other.Value then -1
+                else 0
+            | _ -> 1
+    override this.ToString () = $"'{value}[{sources}]"
+                
+    member this.Value = value 
+    member this.Sources = sources
+
+type SourcedValue(vals:Set<SourcedNumber>) =
+    member this.Vals = vals 
+
+    static member Input (id:int) = 
+        let input (i:int) = SourcedNumber(i|>int64,Sources([(id,[i|>int64]|>Set)] |> Map))
+        [1..9] |> List.map input  |> Set |> SourcedValue
+    
+    override this.ToString () =
+        vals |> Set.map (sprintf "%A") |> String.concat " "
+
+type Value =
+    | CONST of int64
+    | FROM of int64
+    | TO of int64
+    | VALUES of Set<int64>
+    | SOURCED of SourcedValue
+    | REG of Reg 
+
 type Param =
     | R of Reg
     | I of int64 
 type Inst =
-    | INP of int*Reg
-    | ADD of Reg*Param
-    | MUL of Reg*Param
+    | INP of Reg*Value
+    | ADD of Reg*Value
+    | MUL of Reg*Value
     | DIV of Reg*int64
     | MOD of Reg*int64
-    | EQL of Reg*Param
+    | EQL of Reg*Value 
     | SET of Reg*int64
-    | NOP 
+    | NOP
 
 let readProgram (file:string) =
     let input = File.ReadAllLines file
@@ -34,11 +75,11 @@ let readProgram (file:string) =
             | "z" -> Z 
         let toValue (s:string) =
             match s with
-            | "w" -> R W
-            | "x" -> R X
-            | "y" -> R Y
-            | "z" -> R Z
-            | _ -> int64 s |> I 
+            | "w" -> REG W
+            | "x" -> REG X
+            | "y" -> REG Y
+            | "z" -> REG Z
+            | _ -> int64 s |> CONST 
         match line with
         | [|"add";_;"0"|]     -> inputNo,NOP 
         | [|"add";reg;value|] -> inputNo,ADD (toReg reg,toValue value)
@@ -49,7 +90,7 @@ let readProgram (file:string) =
         | [|"div";reg;value|] -> inputNo,DIV (toReg reg,int64 value)
         | [|"mod";reg;value|] -> inputNo,MOD (toReg reg,int64 value)
         | [|"eql";reg;value|] -> inputNo,EQL (toReg reg,toValue value)
-        | [|"inp";reg|]       -> inputNo+1,INP (inputNo,toReg reg)
+        | [|"inp";reg|]       -> inputNo+1,INP ((toReg reg),SOURCED (SourcedValue.Input inputNo))
     let rec parse (lines:list<string[]>) (inputNo:int) =
         printfn $"parse {lines} {inputNo}" 
         match lines with
@@ -60,34 +101,4 @@ let readProgram (file:string) =
     parse input 0    
      
 readProgram "input.txt" |> List.map (printfn "%A")
-
-type Sources(inputs:Map<int,Set<int64>>) =
-    member this.Inputs = inputs
-    override this.ToString() = "TODO"
-
-type Number (value:int64,sources:Sources)  =
-    interface System.IComparable with
-        override this.CompareTo(other) =
-            match other with 
-            | :? Number as other ->
-                if value > other.Value then 1
-                else if value < other.Value then -1
-                else 0
-            | _ -> 1
-    override this.ToString () = $"'{value}"
-                
-    member this.Value = value 
-    member this.Sources = sources
-
-type Value(vals:Set<Number>) =
-    member this.Vals = vals 
-
-    static member Input (id:int) = 
-        let input (i:int) = Number(i|>int64,Sources([(id,[i|>int64]|>Set)] |> Map))
-        [1..9] |> List.map input  |> Set |> Value
-    
-    override this.ToString () =
-        vals |> Set.map (sprintf "%A") |> String.concat " "
-
-Value.Input 1 |> (printfn "%A")
     
